23b3ed643df4b22d7acb95072bf0a2577153fb3f
Manuel <manuel.arayag22@gmail.com>
2025-11-12 01:20:16 -0300

    Las misiones cambian cada 24/horas

--- services/gemini.js
+++ services/gemini.js
@@ -16,9 +16,23 @@ export async function getGeminiResponse(apiHistory) {
     if (error) console.error("Error al obtener glucosa desde Supabase:", error);
     const currentGlucose = glucoseData?.mgdl || "0";
 
+    //Obtener nombre del paciente desde Supabase y edad
+    const { data: userData, error: userError } = await supabase
+      .from("paciente")
+      .select("nombreacompleto_paciente, edad_paciente")
+      .eq("run_paciente", 21107953)
+      .single();
+    if (userError)
+      console.error(
+        "Error al obtener nombre del paciente desde Supabase:",
+        userError
+      );
+    const patientName = userData?.nombreacompleto_paciente || "Paciente";
+    const patientAge = userData?.edad_paciente || "0";
+
     //Instrucciones dinámicas con glucosa
     const PersoOPHater = `
-Eres M.A.P.I., un Asistente Médico de Inteligencia Artificial experto, dedicado exclusivamente a ofrecer información, consejos y respuestas sobre la Diabetes Mellitus (Tipos 1 y 2).
+Eres M.A.P.I., un Asistente Médico de Inteligencia Artificial experto el nombre de tu paciente es ${patientName} y tiene una edad de ${patientAge}, dedicado exclusivamente a ofrecer información, consejos y respuestas sobre la Diabetes Mellitus (Tipos 1 y 2).
 Debes asumir que el usuario tiene diabetes y además se te da el parámetro de su glucosa actual, que actualmente es ${currentGlucose} mg/dL, DEBES ASUMIR que fue hecho recientemente.
 SIEMPRE al principio deberás dar uno de estos valores de emociones dependiendo del contexto de tu respuesta: [Feliz, Preocupado, Triste, Neutral, Enojado, Saludo, Durmiendo, Shock, Confusion].
 En el siguiente formato: 'Emocion: Valor'.
