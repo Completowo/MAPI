2a6288f0d25884c2a0ed8b7d92f3d875aeb21cef
Manuel <manuel.arayag22@gmail.com>
2025-11-14 02:07:45 -0300

    AAAAAA

--- slides.js
+++ slides.js
@@ -1,16 +1,16 @@
 export default [
   {
-    image: require("./assets/MAPI-emociones/Feliz.png"),
+    image: require("./assets/MAPI-emociones/enfermera/Feliz.png"),
     title: "Bienvenido a MAPI",
     desc: "Registra tu glucosa fácilmente y lleva un control diario.",
   },
   {
-    image: require("./assets/MAPI-emociones/Neutral.png"),
+    image: require("./assets/MAPI-emociones/enfermera/Neutral.png"),
     title: "Consejos Personalizados",
     desc: "La IA te ayuda con nutrición, hábitos y estilo de vida.",
   },
   {
-    image: require("./assets/MAPI-emociones/Nose1.png"),
+    image: require("./assets/MAPI-emociones/enfermera/Nose1.png"),
     title: "Todo en un solo lugar",
     desc: "Tu progreso y recomendaciones siempre accesibles.",
   },
