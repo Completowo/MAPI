f29508fd68227ef5b6560a6f60d484844f43dd3a
Manuel <manuel.arayag22@gmail.com>
2025-11-16 02:54:35 -0300

    AHoRA sSi

--- components/BackButton.jsx
+++ components/BackButton.jsx
@@ -1,7 +1,9 @@
 import { Pressable } from "react-native";
 import { BackIcon } from "./Icons";
 
-export function BackButton({ onPress }) {
+export function BackButton({ onPress, visible = true }) {
+  if (!visible) return null;
+
   return (
     <Pressable onPress={onPress}>
       <BackIcon />
